c90053d3fbc2fa2c7d00fe643e27821fe43e0afe
nguyentvu69@gmail.com <nguyentvu69@gmail.com>
2023-03-27 10:10:21 +0700

    upload

---
+++ src/components/ImageDetec.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ZapparCamera, ImageTracker, ZapparCanvas } from '@zappar/zappar-react-three-fiber';
+
+const targetFile = new URL('./assets/github.png', import.meta.url).href;
+
+function ImageDetec() {
+    return (
+      <ZapparCanvas>
+        <ZapparCamera />
+        <ImageTracker
+          onNotVisible={(anchor) => console.log(`Not visible ${anchor.id}`)}
+          onNewAnchor={(anchor) => console.log(`New anchor ${anchor.id}`)}
+          onVisible={(anchor) => console.log(`Visible ${anchor.id}`)}
+          targetImage={targetFile}
+        >
+          <mesh position={[0, 0, -5]}>
+            <sphereBufferGeometry />
+            <meshStandardMaterial color="hotpink" />
+          </mesh>
+        </ImageTracker>
+        <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+      </ZapparCanvas>
+    );
+}
+
+export default ImageDetec;
